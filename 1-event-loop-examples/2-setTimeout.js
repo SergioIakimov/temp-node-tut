31bab19bae34dc683e012b02d7ee591f790d8222
Sergei Iakimov <sergeiyakimov87@gmail.com>
2022-05-23 22:41:37 +0300

    feat:2-await-patterns.js - great approach

---
+++ 1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,7 @@
+//starting operating process
+console.log("first");
+setTimeout(() => {
+  console.log("second");
+}, 0);
+console.log("third");
+//completed and exiting operating system process
--- 9-path-module.js
+++ 9-path-module.js
@@ -1,12 +1,15 @@
 const path = require("path");
 
-console.log(path.sep);
+//console.log(path.sep);
 
 const filePath = path.join("/content/", "subfolder", "text.txt");
-console.log(filePath);
+//console.log(filePath);
 
 const base = path.basename(filePath);
-console.log(base);
+//console.log(base);
 
 const absolute = path.resolve(__dirname, "content", "subfolder", "text.txt");
-console.log(absolute);
+//console.log(absolute);
+
+const txtPath = path.join(__dirname, "content", "subfolder", "text.txt");
+console.log(txtPath);
